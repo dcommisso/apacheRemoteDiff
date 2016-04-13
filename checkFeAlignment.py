aac458e813ae154cdb2de1782611b4a1ece0cd81
Domenico Commisso <domenico.commisso@hypertix.it>
2016-04-13 17:11:18 +0200

    aggiunta funzione purgeip

--- checkFeAlignment.py
+++ checkFeAlignment.py
@@ -4,6 +4,7 @@ from fabric.api import env, run
 from fabric.tasks import execute
 from fabric.context_managers import hide
 import argparse
+import re
 
 parser = argparse.ArgumentParser(description='tool per controllare se i file di configurazione di due Apache in cluster sono allineati')
 parser.add_argument('hosts', help='lista dei nodi apache separati da virgola', metavar='host1,host2')
@@ -22,9 +23,18 @@ if args.p:
 def remotecat(configfile):
     return run('cat %s' % configfile)
 
+def purgeip(configfile):
+    ''' elimina l'indirizzo IP del server dal file di configurazione'''
+    own_ip_regexp = re.compile(r'listen\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', flags=re.IGNORECASE)
+    try:
+        own_ip = own_ip_regexp.search(configfile).group(1)
+        return configfile.replace(own_ip, '')
+    except AttributeError:
+        return configfile
+
 if __name__ == '__main__':
     files_list = []
     for configfile in args.configfiles.split(','):
         with hide('everything'):
             files_list.append(execute(remotecat, configfile))
-    print(files_list[0]['RT-GIUSTIZIA-FE01-P1.rt.tix.it'])
+    print purgeip((files_list[1]['RT-GIUSTIZIA-FE01-P1.rt.tix.it']))
