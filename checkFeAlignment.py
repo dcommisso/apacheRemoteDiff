d33968d16c3ce048220fca48310423c646f9e563
Domenico Commisso <domenico.commisso@hypertix.it>
2016-04-14 16:00:26 +0200

    aggiunte regole per rimuovere tab e spazi multipli in purgejunk

--- checkFeAlignment.py
+++ checkFeAlignment.py
@@ -34,10 +34,14 @@ def purgeip(configfile):
 
 def purgejunk(configfile):
     ''' elimina linee vuote, identazione e commenti'''
-    blank_line = re.compile(r'^\s+', flags = re.MULTILINE)
-    comments = re.compile(r'^\s*#.*', flags = re.MULTILINE)
-    configfile = comments.sub('', configfile)
-    configfile = blank_line.sub('', configfile)
+    blank_line_rule = re.compile(r'^\s+', flags = re.MULTILINE)
+    comments_rule = re.compile(r'^\s*#.*', flags = re.MULTILINE)
+    tabs_rule = re.compile(r'\t+')
+    spaces_rule = re.compile(r' +')
+    configfile = comments_rule.sub('', configfile)
+    configfile = blank_line_rule.sub('', configfile)
+    configfile = tabs_rule.sub(' ', configfile)
+    configfile = spaces_rule.sub(' ', configfile)
     return configfile
 
 if __name__ == '__main__':
@@ -45,6 +49,18 @@ if __name__ == '__main__':
     for configfile in args.configfiles.split(','):
         with hide('everything'):
             files_list.append(execute(remotecat, configfile))
-    a = files_list[0]['RT-GIUSTIZIA-FE01-P1.rt.tix.it']
-    b = files_list[0]['RT-GIUSTIZIA-FE01-P2.rt.tix.it']
-    purgejunk(a)
+    a = files_list[1]['RT-GIUSTIZIA-FE01-P1.rt.tix.it']
+    b = files_list[1]['RT-GIUSTIZIA-FE01-P2.rt.tix.it']
+    a = purgeip(a)
+    a = purgejunk(a)
+    b = purgeip(b)
+    b = purgejunk(b)
+
+
+    
+    result = difflib.unified_diff(a.split('\n'),b.split('\n'))
+    for line in result:
+        print line
+
+    #print(b)
+
